a86fa9a48997a03e1596f3c83164d10d4fcc335f
Vihanga Rashan Silva <vihangarashansilva@gmail.com>
2024-04-06 13:37:20 +0530

    First Deployment: Prepare Webhook with Clerk

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -3,9 +3,10 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { SignedIn } from '@clerk/nextjs'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import { navLinks } from '@/constants'
 import { usePathname } from 'next/navigation'
+import { Button } from '@/components/ui/button'
 
 const Sidebar = () => {
   const pathname = usePathname()
@@ -20,7 +21,7 @@ const Sidebar = () => {
         <nav className="sidebar-nav">
           <SignedIn>
             <ul className="sidebar-nav_elements">
-              {navLinks.map(link => {
+              {navLinks.slice(0, 6).map(link => {
                 const isActive = link.route === pathname;
 
                 return (
@@ -42,7 +43,41 @@ const Sidebar = () => {
                 )
               })}
             </ul>
+
+            <ul className="sidebar-nav_elements">
+              {navLinks.slice(6).map(link => {
+                const isActive = link.route === pathname;
+
+                return (
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group ${isActive ? "bg-purple-gradient text-white" : "text-gray-700"}`}
+                  >
+                    <Link href={link.route} className='sidebar-link'>
+                      <Image
+                        src={link.icon}
+                        alt='logo'
+                        width={24}
+                        height={24}
+                        className={`${isActive && 'brightness-200'}`}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                )
+              })}
+
+              <li className='flex-center cursor-pointer gap-2 p-4'>
+                <UserButton afterSignOutUrl='/' showName />
+              </li>
+            </ul>
           </SignedIn>
+
+          <SignedOut>
+            <Button asChild className='button bg-purple-gradient bg-cover'>
+              <Link href={"/sign-in"}>Login</Link>
+            </Button>
+          </SignedOut>
         </nav>
       </div>
     </aside>
